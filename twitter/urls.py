d0ede173ffc5bf8acaf8dcb42aa4913bfa1c4f0c
Jeff Liang <jeff.liang@hpe.com>
2024-07-04 14:23:00 -0700

    feat: 20~21 notifications apis

--- twitter/urls.py
+++ twitter/urls.py
@@ -20,9 +20,10 @@ from rest_framework import routers
 from accounts.api.views import UserViewSet, AccountViewSet
 from comments.api.views import CommentViewSet
 from friendships.api.views import FriendshipViewSet
+from inbox.api.views import NotificationViewSet
+from likes.api.views import LikeViewSet
 from newsfeeds.api.views import NewsFeedViewSet
 from tweets.api.views import TweetViewSet
-from likes.api.views import LikeViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', UserViewSet)
@@ -32,6 +33,7 @@ router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
 router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
 router.register(r'api/comments', CommentViewSet, basename='comments')
 router.register(r'api/likes', LikeViewSet, basename='likes')
+router.register(r'api/notifications', NotificationViewSet, basename='notifications')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
