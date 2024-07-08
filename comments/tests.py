4acb23347e279f26704ebf399bac410857fd6452
Jeff Liang <jeff.liang@hpe.com>
2024-07-07 22:44:37 -0700

    feat: 30 add cache for user and profile

--- comments/tests.py
+++ comments/tests.py
@@ -4,12 +4,13 @@ from testing.testcases import TestCase
 class CommentModelTests(TestCase):
 
     def setUp(self):
+        self.clear_cache()
         self.linghu = self.create_user('linghu')
         self.tweet = self.create_tweet(self.linghu)
         self.comment = self.create_comment(self.linghu, self.tweet)
 
     def test_comment(self):
-        self.assertNotEqual(comment.__str__(), None)
+        self.assertNotEqual(self.comment.__str__(), None)
 
     def test_like_set(self):
         self.create_like(self.linghu, self.comment)
