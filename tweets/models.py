e86c4dedad986ddf61e13b8ba6df114e3d08c793
Jeff Liang <jeff.liang@hpe.com>
2024-07-15 14:38:22 -0700

    feat: 33 cache tweets list in redis

--- tweets/models.py
+++ tweets/models.py
@@ -5,6 +5,7 @@ from django.db.models.signals import post_save
 
 from likes.models import Like
 from tweets.constants import TweetPhotoStatus, TWEET_PHOTO_STATUS_CHOICES
+from tweets.listeners import push_tweet_to_cache
 from utils.listeners import invalidate_object_cache
 from utils.memcached_helper import MemcachedHelper
 from utils.time_helpers import utc_now
@@ -80,3 +81,4 @@ class TweetPhoto(models.Model):
 
 
 post_save.connect(invalidate_object_cache, sender=Tweet)
+post_save.connect(push_tweet_to_cache, sender=Tweet)
