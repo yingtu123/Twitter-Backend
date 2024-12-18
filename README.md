75c3f73e8438dd52b40067b9ba2201d2166e4c67
Shiriatsu <siriusying716@gmail.com>
2024-12-18 17:39:17 -0500

    My first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# django-twitter-jeff
-I use this github repo to work on the jiuzhang django-twitter.
--- utils/time_helpers.py
+++
@@ -1,5 +0,0 @@
-from datetime import datetime
-import pytz
-
-def utc_now():
-    return datetime.now().replace(tzinfo=pytz.UTC)
