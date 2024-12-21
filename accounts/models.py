429dabca3333d68b4b6552e4c72b6ec8d0cc59b5
Shiriatsu <siriusying716@gmail.com>
2024-12-21 12:47:46 -0500

    Initial Django Project

--- accounts/models.py
+++ accounts/models.py
@@ -1,51 +1,12 @@
-from django.contrib.auth.models import User
 from django.db import models
-from django.db.models.signals import post_save, pre_delete
-
-from accounts.listeners import profile_changed
-from utils.listeners import invalidate_object_cache
-
+from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
-    # One2One field 会创建一个 unique index，确保不会有多个 UserProfile 指向同一个 User
-    user = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
-    # Django 还有一个 ImageField，但是尽量不要用，会有很多的其他问题，用 FileField 可以起到
-    # 同样的效果。因为最后我们都是以文件形式存储起来，使用的是文件的 url 进行访问
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=200, blank=True)
     avatar = models.FileField(null=True)
-    # 当一个 user 被创建之后，会创建一个 user profile 的 object
-    # 此时用户还来不及去设置 nickname 等信息，因此设置 null=True
-    nickname = models.CharField(null=True, max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return '{} {}'.format(self.user, self.nickname)
-
-
-# 定义一个 profile 的 property 方法，植入到 User 这个 model 里
-# 这样当我们通过 user 的一个实例化对象访问 profile 的时候，即 user_instance.profile
-# 就会在 UserProfile 中进行 get_or_create 来获得对应的 profile 的 object
-# 这种写法实际上是一个利用 Python 的灵活性进行 hack 的方法，这样会方便我们通过 user 快速
-# 访问到对应的 profile 信息。
-def get_profile(user):
-    # import 放在函数里面避免循环依赖
-    from accounts.services import UserService
-
-    if hasattr(user, '_cached_user_profile'):
-        return getattr(user, '_cached_user_profile')
-    profile = UserService.get_profile_through_cache(user.id)
-    # 使用 user 对象的属性进行缓存(cache)，避免多次调用同一个 user 的 profile 时
-    # 重复的对数据库进行查询
-    setattr(user, '_cached_user_profile', profile)
-    return profile
-
-
-# 给 User Model 增加了一个 profile 的 property 方法用于快捷访问
-User.profile = property(get_profile)
-
-# hook up with listeners to invalidate cache
-pre_delete.connect(invalidate_object_cache, sender=User)
-post_save.connect(invalidate_object_cache, sender=User)
-
-pre_delete.connect(profile_changed, sender=UserProfile)
-post_save.connect(profile_changed, sender=UserProfile)
--- twitter/urls.py
+++ twitter/urls.py
@@ -1,43 +1,13 @@
-"""twitter URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import include, path
-from rest_framework import routers
+from django.urls import path, include
+from django.http import HttpResponse
 
-from accounts.api.views import UserViewSet, AccountViewSet, UserProfileViewSet
-from comments.api.views import CommentViewSet
-from friendships.api.views import FriendshipViewSet
-from inbox.api.views import NotificationViewSet
-from likes.api.views import LikeViewSet
-from newsfeeds.api.views import NewsFeedViewSet
-from tweets.api.views import TweetViewSet
-
-router = routers.DefaultRouter()
-router.register(r'api/users', UserViewSet)
-router.register(r'api/accounts', AccountViewSet, basename='accounts')
-router.register(r'api/tweets', TweetViewSet, basename='tweets')
-router.register(r'api/friendships', FriendshipViewSet, basename='friendships')
-router.register(r'api/newsfeeds', NewsFeedViewSet, basename='newsfeeds')
-router.register(r'api/comments', CommentViewSet, basename='comments')
-router.register(r'api/likes', LikeViewSet, basename='likes')
-router.register(r'api/notifications', NotificationViewSet, basename='notifications')
-router.register(r'api/profiles', UserProfileViewSet, basename='profiles')
+# 可以添加一个简单的首页视图用于测试
+def index(request):
+    return HttpResponse("Hello, World!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls')),
+    path('', index),  # 添加这行来处理根路径
 ]
