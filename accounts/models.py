639a8f9c2e15225e738dff9e1c4c67924290a5e8
Shiriatsu <siriusying716@gmail.com>
2024-12-26 19:59:37 -0500

    Initial commit with my code

---
+++ accounts/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    nickname = models.CharField(max_length=200, blank=True)
+    avatar = models.FileField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.user, self.nickname)
