2cd8ad48937ed572593b6c65514a976b693e669f
Shiriatsu <siriusying716@gmail.com>
2025-01-03 19:07:35 -0500

    Login, loginstatus, logout, signup API

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -3,9 +3,48 @@ from rest_framework import serializers, exceptions
 
 
 
-class UserSerializer(serializers. ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('username','email')
 
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField( )
+
+    def validate(self, data):
+        if not User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                "username":"This username does not exist."
+            })
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20,min_length=6)
+    password = serializers.CharField(max_length=20,min_length=6)
+    email=serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+    def validate(self, data):
+        # TODO<HOMEWORK>: 增加验证 username 是不是只由给定的字符集合构成
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'This username  has been occupied.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'email': 'This email address has been occupied.'
+            })
+        return data
+
+    def create(self, validated_data):
+        username = validated_data['username'].lower()
+        email = validated_data['email'].lower()
+        password = validated_data['password']
+        user = User.objects.create_user(username=username, email=email, password=password)
+        return user
 
